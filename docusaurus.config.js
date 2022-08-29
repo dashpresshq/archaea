122f16d90f766fb7454908f370cd011ff81ad678
Ayobami Akingbade <akingbadefred@gmail.com>
2022-08-29 21:32:32 +0100

    docs: disable blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,9 +7,9 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Hadmean",
-  tagline: "Dinosaurs are cool",
-  url: "https://hadmean.com",
-  baseUrl: "/",
+  tagline: "Super Admin",
+  url: "https://hadmean.github.io",
+  baseUrl: "/hadmean/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
@@ -17,6 +17,8 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "hadmean",
   projectName: "hadmean",
+
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -35,16 +37,9 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/hadmean/archaea/edit/main",
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-        },
+        blog: false,
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
@@ -68,7 +63,7 @@ const config = {
             position: "left",
             label: "Documentation",
           },
-          { to: "/blog", label: "Blog", position: "left" },
+          // { to: "/blog", label: "Blog", position: "left" },
           {
             href: "https://github.com/hadmean/hadmean",
             label: "GitHub",
