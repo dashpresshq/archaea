de9500a26f97875feef8d9eba244adc4f3628518
Ayobami Akingbade <akingbadefred@gmail.com>
2022-10-27 01:18:49 +0100

    stating the features

--- src/screens/Home/Features.jsx
+++ src/screens/Home/Features.jsx
@@ -1,22 +1,148 @@
 import React from "react";
 import Link from "@docusaurus/Link";
 
+const FEATURES = [
+  {
+    title: "Open Source",
+    subTitle: " Modify and install how you see fit.",
+    description: `You don't want to trust strangers with your precious data as Hadmean
+    is fully open-source. `,
+    svg: (
+      <svg
+        className="w-10 h-10 mb-2 text-primary md:w-12 md:h-12 dark:text-primary-light"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fillRule="evenodd"
+          d="M2 5a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2V5zm14 1a1 1 0 11-2 0 1 1 0 012 0zM2 13a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2v-2zm14 1a1 1 0 11-2 0 1 1 0 012 0z"
+          clipRule="evenodd"
+        ></path>
+      </svg>
+    ),
+  },
+  {
+    title: "Customizable",
+    subTitle: "Everything you see can be configured",
+    description: `
+    You want something extremely customizable that also gives you the reign when your requirements become very unique.
+ 
+    `,
+    svg: (
+      <svg
+        className="w-10 h-10 mb-2 text-primary md:w-12 md:h-12 dark:text-primary-light"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fillRule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zM4.332 8.027a6.012 6.012 0 011.912-2.706C6.512 5.73 6.974 6 7.5 6A1.5 1.5 0 019 7.5V8a2 2 0 004 0 2 2 0 011.523-1.943A5.977 5.977 0 0116 10c0 .34-.028.675-.083 1H15a2 2 0 00-2 2v2.197A5.973 5.973 0 0110 16v-2a2 2 0 00-2-2 2 2 0 01-2-2 2 2 0 00-1.668-1.973z"
+          clipRule="evenodd"
+        ></path>
+      </svg>
+    ),
+  },
+  {
+    title: "Agnostic",
+    subTitle: " Your language or framework doesn't matter",
+    description: ` You want an internal tool that will work regardless of you
+    programming language/framework.`,
+    svg: (
+      <svg
+        className="w-10 h-10 mb-2 text-primary md:w-12 md:h-12 dark:text-primary-light"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>
+      </svg>
+    ),
+  },
+  {
+    title: "Easy To Use",
+    subTitle: "No Tutorial",
+    description: `You want an internal tools that don't require techincal knowledge to make changes`,
+    svg: (
+      <svg
+        className="w-10 h-10 mb-2 text-primary md:w-12 md:h-12 dark:text-primary-light"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z"></path>
+      </svg>
+    ),
+  },
+  {
+    title: "Simple Installation",
+    subTitle: "Just one command",
+    description: `You want an internal tools that you can fully setup and start using under 59 seconds`,
+    svg: (
+      <svg
+        className="w-10 h-10 mb-2 text-primary md:w-12 md:h-12 dark:text-primary-light"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z"></path>
+      </svg>
+    ),
+  },
+  {
+    title: "Declarative",
+    subTitle: "Less Maintenance and No Logic Bugs",
+    description: `You want an internal tool that is auto-generated which means zero maintenance zero logic bugs.`,
+    svg: (
+      <svg
+        className="w-10 h-10 mb-2 text-primary md:w-12 md:h-12 dark:text-primary-light"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z"></path>
+      </svg>
+    ),
+  },
+];
+
 export function Features() {
   return (
     <section className="bg-white dark:bg-gray-900">
-      <div className="items-center max-w-screen-xl px-4 py-8 mx-auto lg:grid lg:grid-cols-4 lg:gap-16 xl:gap-24 lg:py-24 lg:px-6">
-        <div className="col-span-2 mb-8">
+      <div className="items-center max-w-screen-xl px-4 py-8 mx-auto">
+        <div className="mb-8 text-center">
           <p className="text-lg font-medium text-primary dark:text-primary-light">
             Why you should try Hadmean
           </p>
           <h2 className="mt-3 mb-4 text-3xl font-extrabold tracking-tight text-gray-900 md:text-3xl dark:text-white">
-            Trusted by over 600 million users and 10,000 teams
+            You want powerful internal tools without the headache of building
+            and maintenance.
           </h2>
-          <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">
-            Hadmean is an internal tool generator. It is language agnostic,
-            schema driven, extremely customizable, fully featured, user-friendly
-            and has just one installation step.
-          </p>
+        </div>
+        <div className="space-y-8 md:grid md:grid-cols-3 md:gap-12 md:space-y-0">
+          {FEATURES.map(({ title, description, subTitle, svg }) => (
+            <div key={title} className="text-center">
+              {svg}
+              <h3 className="mb-2 text-2xl font-bold dark:text-white">
+                {title}
+              </h3>
+              <p className="font-light text-gray-500 dark:text-gray-400">
+                {subTitle}
+              </p>
+              <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">
+                {description}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+{
+  /* 
           <div className="pt-6 mt-6 space-y-4 border-t border-gray-200 dark:border-gray-700">
             <div>
               <Link
@@ -38,83 +164,5 @@ export function Features() {
                 </svg>
               </Link>
             </div>
-          </div>
-        </div>
-        <div className="col-span-2 space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0">
-          <div>
-            <svg
-              className="w-10 h-10 mb-2 text-primary md:w-12 md:h-12 dark:text-primary-light"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M2 5a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2V5zm14 1a1 1 0 11-2 0 1 1 0 012 0zM2 13a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2v-2zm14 1a1 1 0 11-2 0 1 1 0 012 0z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-            <h3 className="mb-2 text-2xl font-bold dark:text-white">
-              Open Source
-            </h3>
-            <p className="font-light text-gray-500 dark:text-gray-400">
-              Modify and install how you see fit.
-            </p>
-          </div>
-          <div>
-            <svg
-              className="w-10 h-10 mb-2 text-primary md:w-12 md:h-12 dark:text-primary-light"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z"></path>
-            </svg>
-            <h3 className="mb-2 text-2xl font-bold dark:text-white">
-              Tons of features
-            </h3>
-            <p className="font-light text-gray-500 dark:text-gray-400">
-              All built and wired
-            </p>
-          </div>
-          <div>
-            <svg
-              className="w-10 h-10 mb-2 text-primary md:w-12 md:h-12 dark:text-primary-light"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zM4.332 8.027a6.012 6.012 0 011.912-2.706C6.512 5.73 6.974 6 7.5 6A1.5 1.5 0 019 7.5V8a2 2 0 004 0 2 2 0 011.523-1.943A5.977 5.977 0 0116 10c0 .34-.028.675-.083 1H15a2 2 0 00-2 2v2.197A5.973 5.973 0 0110 16v-2a2 2 0 00-2-2 2 2 0 01-2-2 2 2 0 00-1.668-1.973z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-            <h3 className="mb-2 text-2xl font-bold dark:text-white">
-              Customizable
-            </h3>
-            <p className="font-light text-gray-500 dark:text-gray-400">
-              <b>Everything</b> you see can be configured
-            </p>
-          </div>
-          <div>
-            <svg
-              className="w-10 h-10 mb-2 text-primary md:w-12 md:h-12 dark:text-primary-light"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>
-            </svg>
-            <h3 className="mb-2 text-2xl font-bold dark:text-white">
-              Agnostic
-            </h3>
-            <p className="font-light text-gray-500 dark:text-gray-400">
-              Your language or framework doesn't matter
-            </p>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+          </div> */
 }
--- src/screens/Home/Hero.jsx
+++ src/screens/Home/Hero.jsx
@@ -10,7 +10,7 @@ export function Hero() {
       <div className="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
         <div className="mr-auto place-self-center lg:col-span-7">
           <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
-            Build powerful internal tools under 59 seconds
+            Setup powerful internal tools under 59 seconds
           </h1>
           <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
             <span className="text-lg font-medium text-primary dark:text-primary-light">
