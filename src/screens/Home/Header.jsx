48f49fabba0dfd8497281691a86ce997933013a2
Ayobami Akingbade <akingbadefred@gmail.com>
2022-10-30 17:15:17 +0100

    fix large icons on ios

--- src/screens/Home/Header.jsx
+++ src/screens/Home/Header.jsx
@@ -47,7 +47,7 @@ export function Header() {
             </Link>
             <Link
               href="https://discord.gg/aV6DxwXhzN"
-              className="text-primary dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-2 py-2 lg:py-2.5 sm:mr-2 focus:outline-none dark:focus:ring-gray-800"
+              className="text-primary dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-2 py-2 lg:py-2.5 mr-2 focus:outline-none dark:focus:ring-gray-800"
             >
               <svg
                 className="w-5 h-5"
@@ -71,7 +71,7 @@ export function Header() {
             <button
               data-collapse-toggle="mobile-menu-2"
               type="button"
-              className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="inline-flex items-center pl-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               aria-controls="mobile-menu-2"
               aria-expanded="false"
               onClick={() => {
