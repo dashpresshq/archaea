1f4cb29a0bd774a24f6b2f178858026b6ae66204
Ayobami Akingbade <akingbadefred@gmail.com>
2022-10-30 22:07:15 +0100

    push faq up

--- src/screens/Home/Faq.jsx
+++ src/screens/Home/Faq.jsx
@@ -30,7 +30,7 @@ export function Faq() {
   const [current, setCurrent] = useState(0);
   return (
     <section className="bg-white dark:bg-gray-900">
-      <div className="max-w-screen-xl px-4 pb-8 mx-auto lg:p-24 lg:px-6 ">
+      <div className="max-w-screen-xl px-4 py-8 mx-auto lg:p-12 lg:px-6 ">
         <h2 className="mb-6 text-3xl font-extrabold tracking-tight text-center text-gray-900 lg:mb-8 lg:text-3xl dark:text-white">
           Frequently asked questions
         </h2>
